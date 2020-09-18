226b2ea008d1475470d60057cd89a1d08f046aef
chenmeifeng <18410105520@163.com>
2020-09-18 16:55:25 +0800

    提交门店列表

--- wisdom-business/src/view/settlement/doorStore/performanceSetting.vue
+++ wisdom-business/src/view/settlement/doorStore/performanceSetting.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="storeList">
-    业绩设置
+  <div class="performanceSetting  flexColumn">
+    <el-tabs v-model="activeName"
+             style="flex: 1"
+             class="flexColumn"
+             @tab-click="handleClick">
+      <el-tab-pane label="基础设置"
+                   name="first">
+        <div class="conent_box">
+
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="配置管理"
+                   name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理"
+                   name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿"
+                   name="fourth">定时任务补偿</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name: ''
+  name: 'performanceSetting',
+  data () {
+    return {
+      activeName: 'first'
+    };
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event);
+    }
+  }
 }
 </script>
 
--- wisdom-business/src/view/settlement/doorStore/storeList.vue
+++ wisdom-business/src/view/settlement/doorStore/storeList.vue
@@ -1,14 +1,391 @@
 <template>
-  <div class="storeList">
-    门店列表
+  <div class="storeList flexColumn">
+    <el-tabs v-model="activeName"
+             style="flex: 1"
+             class="flexColumn"
+             @tab-click="handleClick">
+      <el-tab-pane label="门店列表"
+                   name="first">
+        <div class="conent_box flexColumn"
+             style="height: 100%">
+          <div class="flexJC">
+            <p class="text">页面列表</p>
+          </div>
+          <div class="flexColumn conent_box"
+               style="flex: 1">
+            <div class="c_box">
+              <div class="flexJC ">
+                <div>
+                  <el-select v-model="type"
+                             style="width: 100px"
+                             slot="prepend"
+                             size="mini"
+                             placeholder="请选择">
+                    <el-option label="按地区筛选"
+                               value="1"></el-option>
+                    <el-option label="按区域筛选"
+                               value="2"></el-option>
+                  </el-select>
+                  <el-select style="width: 100px;margin-left: 0"
+                             size="mini"
+                             slot="prepend"
+                             placeholder="请选择地区">
+                  </el-select>
+                </div>
+
+                <div>
+                  <el-input placeholder="请输入内容"
+                            style="width: 300px; margin: 0"
+                            size="mini"
+                            v-model="search"
+                            class="input-with-select ml">
+                    <el-select v-model="sleValue"
+                               style="width: 100px"
+                               slot="prepend"
+                               placeholder="请选择">
+                      <el-option label="门店名称"
+                                 value="1"></el-option>
+                      <el-option label="门店具体位置"
+                                 value="2"></el-option>
+                      <el-option label="门店编号"
+                                 value="3"></el-option>
+                    </el-select>
+                    <el-button slot="append"
+                               size="mini"
+                               icon="el-icon-search"></el-button>
+                  </el-input>
+
+                  <el-button size="mini"
+                             type="primary"
+                             plain>高级筛选</el-button>
+                </div>
+
+              </div>
+
+              <div class="flexRC mt">
+                <p class="minText">商品标签</p>
+                <div class="button ml mr">不限</div>
+                <el-button size="mini">添加标签</el-button>
+              </div>
+
+              <el-divider></el-divider>
+              <div class="flex mt">
+                <p class="minText mr">快捷操作</p>
+                <el-button size="mini">新增门店</el-button>
+                <el-button size="mini">批量导入</el-button>
+                <el-button size="mini">导出门店</el-button>
+                <el-button size="mini">打标签</el-button>
+                <el-button size="mini">批量设置</el-button>
+                <el-select v-model="stateValue"
+                           class="ml"
+                           style="width: 100px;"
+                           size="mini"
+                           clearable
+                           placeholder="请选择">
+                  <el-option v-for="item in state"
+                             :key="item.value"
+                             :label="item.label"
+                             :value="item.value">
+                  </el-option>
+                </el-select>
+              </div>
+            </div>
+
+            <div class="table"
+                 style="flex: 1">
+              <el-table :data="tableData"
+                        stripe
+                        style="width: 100%">
+                <el-table-column prop="name"
+                                 show-overflow-tooltip
+                                 label="门店编号"
+                                 width="180">
+                </el-table-column>
+                <el-table-column prop="address"
+                                 show-overflow-tooltip
+                                 label="门店名称">
+                </el-table-column>
+                <el-table-column prop="address"
+                                 show-overflow-tooltip
+                                 label="门店地址">
+                </el-table-column>
+                <el-table-column prop="address"
+                                 show-overflow-tooltip
+                                 label="门店排序">
+                </el-table-column>
+                <el-table-column prop="address"
+                                 show-overflow-tooltip
+                                 label="门店状态">
+                </el-table-column>
+                <el-table-column show-overflow-tooltip
+                                 label="操作"
+                                 width="270"
+                                 min-width="60">
+                  <template slot-scope="scope">
+                    <div>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor right20"
+                                 @click="look(scope.$index, scope.row)">编辑</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor right20"
+                                 @click="edit(scope.$index, scope.row)">投放</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor"
+                                 @click="checkTrackQueryFun(scope.$index, scope.row)">打标签</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor"
+                                 @click="checkTrackQueryFun(scope.$index, scope.row)">分配门店</el-button>
+                      <!-- <el-button size="medium"
+                           type="text"
+                           class="redColor"
+                           @click="checkTrackQueryFun(scope.$index, scope.row)">打标签</el-button> -->
+                    </div>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+
+            <div class="btootm_paination c_box mt">
+              <!-- <el-pagination @current-change="handleCurrentChangeFun"
+                         :hide-on-single-page="false"
+                         :current-page="currentPage"
+                         layout="total, jumper,  ->, prev, pager, next"
+                         :total="totalData"></el-pagination> -->
+              <el-pagination @size-change="handleSizeChange"
+                             @current-change="handleCurrentChangeFun"
+                             :current-page="currentPage"
+                             :page-sizes="[100, 200, 300, 400]"
+                             :page-size="100"
+                             layout="total, sizes, prev, pager, next, jumper"
+                             :total="400">
+              </el-pagination>
+            </div>
+          </div>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="门店标签"
+                   name="second">
+        <div class="conent_box flexColumn"
+             style="height: 100%">
+          <div class="flexJC">
+            <p class="text">页面列表</p>
+          </div>
+          <div class="flexColumn conent_box"
+               style="flex: 1">
+            <div class="c_box">
+              <div class="flex">
+                <el-button size="mini">新增标签</el-button>
+                <el-button size="mini">批量删除</el-button>
+              </div>
+            </div>
+
+            <div class="table"
+                 style="flex: 1">
+              <el-table :data="tableData"
+                        stripe
+                        style="width: 100%">
+                <el-table-column prop="name"
+                                 show-overflow-tooltip
+                                 label="门店标签"
+                                 width="180">
+                </el-table-column>
+                <el-table-column prop="address"
+                                 show-overflow-tooltip
+                                 label="使用门店数量">
+                </el-table-column>
+                <el-table-column show-overflow-tooltip
+                                 label="操作"
+                                 width="270"
+                                 min-width="60">
+                  <template slot-scope="scope">
+                    <div>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor right20"
+                                 @click="look(scope.$index, scope.row)">编辑</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor right20"
+                                 @click="edit(scope.$index, scope.row)">投放</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor"
+                                 @click="checkTrackQueryFun(scope.$index, scope.row)">打标签</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor"
+                                 @click="checkTrackQueryFun(scope.$index, scope.row)">分配门店</el-button>
+                      <!-- <el-button size="medium"
+                           type="text"
+                           class="redColor"
+                           @click="checkTrackQueryFun(scope.$index, scope.row)">打标签</el-button> -->
+                    </div>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+
+            <div class="btootm_paination c_box mt">
+              <!-- <el-pagination @current-change="handleCurrentChangeFun"
+                         :hide-on-single-page="false"
+                         :current-page="currentPage"
+                         layout="total, jumper,  ->, prev, pager, next"
+                         :total="totalData"></el-pagination> -->
+              <el-pagination @size-change="handleSizeChange"
+                             @current-change="handleCurrentChangeFun"
+                             :current-page="currentPage"
+                             :page-sizes="[100, 200, 300, 400]"
+                             :page-size="100"
+                             layout="total, sizes, prev, pager, next, jumper"
+                             :total="400">
+              </el-pagination>
+            </div>
+          </div>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="门店权限"
+                   name="third">
+        <div class="conent_box flexColumn"
+             style="height: 100%">
+          <div class="flexJC">
+            <p class="text">页面列表</p>
+          </div>
+          <div class="flexColumn conent_box"
+               style="flex: 1">
+            <div class="c_box">
+              <div class="flex">
+                <el-button size="mini">新增权限</el-button>
+              </div>
+            </div>
+
+            <div class="table"
+                 style="flex: 1">
+              <el-table :data="tableData"
+                        stripe
+                        style="width: 100%">
+                <el-table-column prop="name"
+                                 show-overflow-tooltip
+                                 label="门店权限名"
+                                 width="180">
+                </el-table-column>
+                <el-table-column prop="address"
+                                 show-overflow-tooltip
+                                 label="使用门店数量">
+                </el-table-column>
+                <el-table-column show-overflow-tooltip
+                                 label="操作"
+                                 width="270"
+                                 min-width="60">
+                  <template slot-scope="scope">
+                    <div>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor right20"
+                                 @click="look(scope.$index, scope.row)">编辑</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor right20"
+                                 @click="edit(scope.$index, scope.row)">投放</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor"
+                                 @click="checkTrackQueryFun(scope.$index, scope.row)">打标签</el-button>
+                      <el-button size="medium"
+                                 type="text"
+                                 class="blueColor"
+                                 @click="checkTrackQueryFun(scope.$index, scope.row)">分配门店</el-button>
+                      <!-- <el-button size="medium"
+                           type="text"
+                           class="redColor"
+                           @click="checkTrackQueryFun(scope.$index, scope.row)">打标签</el-button> -->
+                    </div>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+
+            <div class="btootm_paination c_box mt">
+              <!-- <el-pagination @current-change="handleCurrentChangeFun"
+                         :hide-on-single-page="false"
+                         :current-page="currentPage"
+                         layout="total, jumper,  ->, prev, pager, next"
+                         :total="totalData"></el-pagination> -->
+              <el-pagination @size-change="handleSizeChange"
+                             @current-change="handleCurrentChangeFun"
+                             :current-page="currentPage"
+                             :page-sizes="[100, 200, 300, 400]"
+                             :page-size="100"
+                             layout="total, sizes, prev, pager, next, jumper"
+                             :total="400">
+              </el-pagination>
+            </div>
+          </div>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name: ''
+  name: '',
+  data () {
+    return {
+      activeName: 'first',
+      type: '1',
+      state: [{
+        value: '1',
+        label: '更多'
+      }, {
+        value: '2',
+        label: '修改装修权限'
+      }, {
+        value: '3',
+        label: '修改线上库存模式'
+      }, {
+        value: '4',
+        label: '修改线下库存模式'
+      }, {
+        value: '5',
+        label: '修改门店权限'
+      }, {
+        value: '6',
+        label: '修改发票模版'
+      }, {
+        value: '7',
+        label: '修改分成方案'
+      }],
+    };
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event);
+    }
+  }
 }
 </script>
 
+<style lang="scss">
+.el-tabs .el-tabs__content {
+  flex: 1;
+  .el-tab-pane {
+    height: 100%;
+  }
+}
+</style>
 <style lang="scss" scoped>
+.button {
+  background: #d3e7ff !important;
+  color: #2589ff !important;
+  width: 90px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  border-radius: 4px;
+  font-size: 12px;
+}
 </style>
